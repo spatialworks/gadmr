0cfd3a1ae9033737e78d4e94e5253012098b49bc
Ernest Guevarra <ernest@validmeasures.org>
2018-11-29 22:31:21 +0000

    update get_map.R

---
+++ R/get_map.R
@@ -0,0 +1,79 @@
+################################################################################
+#
+#' get_geopackage
+#'
+#' Get geopackage format map of a specific country from GADM.
+#'
+#' @param country Three-letter ISO country code. Corresponding three-letter ISO
+#'     code for each country can be found in the \code{list_countries} dataset.
+#' @param version A character vector specifying the GADM version from which to
+#'     get the geopackage download from. Default is \code{gadm3.6} for current
+#'     version of GADM.
+#' @param layer A numeric value specifying which layer from geopackage to get.
+#'     A layer corresponds to the different administrative units of the
+#'     specific country where 1 is country-level.
+#'
+#' @return SpatialPolygonsDataFrame of the specified country map layer.
+#'
+#' @examples
+#'
+#' get_geopackage(country = "AFG", layer = 1)
+#'
+#' @export
+#'
+#
+################################################################################
+
+get_geopackage <- function(country, version = "gadm3.6", layer){
+  temp <- tempfile()
+
+  url <- paste("https://biogeo.ucdavis.edu/data/", version, "/gpkg/",
+               stringr::str_remove(string = version, pattern = "\\."),
+               "_", country, "_gpkg.zip", sep = "")
+
+  download.file(url, temp)
+
+  unzip(temp, exdir = tempdir())
+
+  dsn <- paste(tempdir(), "/",
+               stringr::str_remove(string = version, pattern = "\\."),
+               "_", country, ".gpkg", sep = "")
+
+  layers <- rgdal::ogrListLayers(dsn = dsn)
+
+  if(layer > length(layers)) stop(paste("Geopackage has only ",
+                                        length(layers),
+                                        " layers. Specify layer from 1 to ",
+                                        length(layers),
+                                        ". Try again.",
+                                        sep = ""),
+                                  call. = TRUE)
+
+  gpkg <- rgdal::readOGR(dsn = paste(tempdir(), "/",
+                                     stringr::str_remove(string = version, pattern = "\\."),
+                                     "_", country, ".gpkg", sep = ""),
+                         layer = layers[layer])
+
+  unlink(temp)
+
+  return(gpkg)
+}
+
+
+
+
+################################################################################
+#
+#' get_map
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#
+################################################################################
