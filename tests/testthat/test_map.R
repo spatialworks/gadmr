cce922c7c7c53161be7b506b01a795fc3742b1b4
Ernest Guevarra <ernest@validmeasures.org>
2018-11-30 13:08:13 +0000

    create new test

---
+++ tests/testthat/test_map.R
@@ -0,0 +1,22 @@
+library(gadmr)
+context("Test map")
+
+afg_map_0 <- get_map(format = "gpkg", country = "AFG", layer = 1)
+afg_map_1 <- get_map(format = "gpkg", country = "AFG", layer = 2)
+afg_map_2 <- get_map(format = "gpkg", country = "AFG", layer = 3)
+
+test_that("afg_map is SpatialPolygonsDataFrame", {
+  expect_is(afg_map_0, "SpatialPolygonsDataFrame")
+  expect_is(afg_map_1, "SpatialPolygonsDataFrame")
+  expect_is(afg_map_2, "SpatialPolygonsDataFrame")
+})
+
+afg_map_0 <- get_map(format = "shp", country = "AFG", layer = 1)
+afg_map_1 <- get_map(format = "shp", country = "AFG", layer = 2)
+afg_map_2 <- get_map(format = "shp", country = "AFG", layer = 3)
+
+test_that("afg_map is SpatialPolygonsDataFrame", {
+  expect_is(afg_map_0, "SpatialPolygonsDataFrame")
+  expect_is(afg_map_1, "SpatialPolygonsDataFrame")
+  expect_is(afg_map_2, "SpatialPolygonsDataFrame")
+})
