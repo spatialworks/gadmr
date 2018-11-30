f702e8163b2dd4b1b93e5919c3732f9edab0477c
Ernest Guevarra <ernest@validmeasures.org>
2018-11-30 13:22:42 +0000

    add tests

--- tests/testthat/test_geopackage.R
+++ tests/testthat/test_geopackage.R
@@ -8,3 +8,8 @@ afg_map_2 <- get_geopackage(country = "AFG", layer = 3)
 test_that("afg_map is SpatialPolygonsDataFrame", {
   expect_is(afg_map_0, "SpatialPolygonsDataFrame")
 })
+
+test_that("Produce an error message at layer = 4", {
+  expect_error(get_geopackage(country = "AFG", layer = 4))
+})
+
