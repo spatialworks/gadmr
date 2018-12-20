a7ffdb6dcb988f050620ca68a1d961188a00b645
Ernest Guevarra <ernest@validmeasures.org>
2018-12-20 12:15:29 +0000

    update tests

--- tests/testthat/test_shapefile.R
+++ tests/testthat/test_shapefile.R
@@ -1,9 +1,9 @@
 library(gadmr)
 context("Test shapefile")
 
-afg_map_0 <- get_shapefile(country = "AFG", layer = 1)
-afg_map_1 <- get_shapefile(country = "AFG", layer = 2)
-afg_map_2 <- get_shapefile(country = "AFG", layer = 3)
+afg_map_0 <- get_shapefile(country = "AFG", layer = 0)
+afg_map_1 <- get_shapefile(country = "AFG", layer = 1)
+afg_map_2 <- get_shapefile(country = "AFG", layer = 2)
 
 test_that("afg_map is SpatialPolygonsDataFrame", {
   expect_is(afg_map_0, "SpatialPolygonsDataFrame")
